40e90b7b6d451c1fa3c42f5f0a62b389260b1353
Yura Subotinov <crapmindd@gmail.com>
2025-06-11 15:07:12 +0300

    Added Hibernate configurations

--- src/main/java/com/game/repository/PlayerRepositoryDB.java
+++ src/main/java/com/game/repository/PlayerRepositoryDB.java
@@ -2,16 +2,32 @@ package com.game.repository;
 
 import com.game.entity.Player;
 import jakarta.annotation.PreDestroy; // the annotation package was changed due to developers moving it
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Properties;
 
 @Repository(value = "db")
 public class PlayerRepositoryDB implements IPlayerRepository {
 
-    public PlayerRepositoryDB() {
+    private final SessionFactory sessionFactory;
 
+    public PlayerRepositoryDB() {
+        Properties properties = new Properties();
+
+        String username = System.getProperty("USER_NAME");
+        String password = System.getProperty("USER_PASS");
+
+        properties.put("hibernate.connection.username", username);
+        properties.put("hibernate.connection.password", password);
+
+        Configuration configuration = new Configuration()
+                .addProperties(properties);
+
+        this.sessionFactory = configuration.configure().buildSessionFactory();
     }
 
     @Override
@@ -46,5 +62,9 @@ public class PlayerRepositoryDB implements IPlayerRepository {
     @PreDestroy
     public void beforeStop() {
 
+    }
+
+    public SessionFactory getSessionFactory() {
+        return sessionFactory;
     }
 }
